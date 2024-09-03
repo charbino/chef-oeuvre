281c914f384afa71d0d620e6021d3bc8fd08b179
Sébastien Framinet <sebastien@next-xp.com>
2024-09-03 16:53:43 +0200

    Install php stan extension symfony + rector

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonySetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    //$rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+
+    $rectorConfig->paths([
+        __DIR__ . '/config',
+        __DIR__ . '/migrations',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::PHP_82,
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ]);
+};
