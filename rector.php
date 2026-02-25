93ca73ce89f4997dea450eaf65d01233e28ffbbf
Sébastien Framinet <sebastien@next-xp.com>
2026-02-25 17:13:21 +0100

    update sf 7.4

--- rector.php
+++ rector.php
@@ -3,30 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    //$rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
-
-    $rectorConfig->paths([
-        __DIR__ . '/config',
-        __DIR__ . '/migrations',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::PHP_82,
-        SymfonySetList::SYMFONY_60,
-        SymfonySetList::SYMFONY_61,
-        SymfonySetList::SYMFONY_62,
-        SymfonySetList::SYMFONY_63,
-        SymfonySetList::SYMFONY_64,
-        SymfonySetList::SYMFONY_70,
-        SymfonySetList::SYMFONY_71,
-    ]);
-};
+return RectorConfig::configure()
+    ->withPreparedSets(symfonyCodeQuality: true)
+    ->withComposerBased(symfony: true);
